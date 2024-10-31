8ed53b49c9c7b45a4bfd612b8f43c5e13b343f4c
seb54000 <seb54000@gmail.com>
2024-10-31 18:24:03 +0100

    default browser shortcut

--- terraform-infra/cloudinit/user_data_tpiac.sh
+++ terraform-infra/cloudinit/user_data_tpiac.sh
@@ -135,7 +135,7 @@ echo "Install Chrome"
 # sudo snap install chromium
 
 sudo apt install -y chromium-bsu
-echo "Install CHromimum Extension (auto refresh)"
+echo "Install Chromium Extension (auto refresh)"
 cat <<EOF > /var/tmp/autorefresh.json
 {
     "ExtensionInstallForcelist":
@@ -145,6 +145,9 @@ cat <<EOF > /var/tmp/autorefresh.json
 EOF
 sudo mkdir -p /var/snap/chromium/current/policies/managed
 sudo mv /var/tmp/autorefresh.json /var/snap/chromium/current/policies/managed/autorefresh.json
+
+# Manage xfce browser shortcut to use chromium
+sed -i s/Exec.*/Exec=chromium/g /usr/share/applications/xfce4-web-browser.desktop
 
 echo "Start some tools when opening XRDP session"
 sudo su - vm${count_number_2digits} -c "mkdir -p /home/vm${count_number_2digits}/.config/autostart/"
